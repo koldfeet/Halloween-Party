f41097a47bd4daa27a4e7075f4407b502917fcfd
Aaron Menocal <2001menocal@gmail.com>
2021-10-26 20:20:02 -0600

    java

---
+++ app.js
@@ -0,0 +1,20 @@
+console.log("hello"+"5*10");
+console.log("hello"+"world");
+alert("You have to be 18 to enter this site");
+confirm("Are you over 18");
+prompt("How old are you")
+var name= "Aaron"
+var pizzaCount= "5"
+var totalCost= "5$"
+var isHungry= "true"
+var firstname= "Aaron"
+var lastname= "Menocal"
+console.log(firstname)
+alert("My name is"+" "+firstname);
+document.getElementById("demo").innerHTML="My name is"+""+firstname;
+console.log("My name is"+" "+firstname);
+document.write("My name is"+" "+firstname);
+alert("My name is"+" "+firstname);
+console.log(firstName[5]);
+alert("My name is") + "" + firstName + '\n' + "My last name is" + "" + lastName + '\n' +
+    "My last name is" + "" + lastName ;
